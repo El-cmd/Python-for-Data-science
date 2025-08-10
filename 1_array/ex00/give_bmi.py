c1301eb50502ce1cd704ef6ac627dfb66cdd0ccd
El-cmd <lothvalentin@gmail.com>
2025-08-10 23:58:08 +0200

    first exo

---
+++ 1_array/ex00/give_bmi.py
@@ -0,0 +1,48 @@
+def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
+    """
+    Calcule l'IMC pour chaque paire (taille, poids).
+
+    height: liste de tailles en mètres
+    weight: liste de poids en kg
+    return: liste des IMC
+    """
+    if not isinstance(height, list) or not isinstance(weight, list):
+        raise TypeError("height and weight must be lists")
+    if len(height) != len(weight):
+        raise ValueError("height and weight lists must have the same length")
+    for h in height:
+        if not isinstance(h, (int, float)):
+            raise TypeError("all height values must be int or float")
+        if h <= 0:
+            raise ValueError("height values must be positive")
+    for w in weight:
+        if not isinstance(w, (int, float)):
+            raise TypeError("all weight values must be int or float")
+        if w <= 0:
+            raise ValueError("weight values must be positive")
+    bmi = []
+    for i in range(len(height)):
+        bmi.append(weight[i] / (height[i] ** 2))
+    return bmi
+
+def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
+    """
+    Retourne True si l'IMC est au-dessus de limit, sinon False.
+
+    bmi: liste des IMC
+    limit: seuil
+    """
+    if not isinstance(bmi, list):
+        raise TypeError("bmi must be a list")
+    for bmi in bmi:
+        if not isinstance(bmi, (int, float)):
+            raise TypeError("all elements in bmi_list must be int or float")
+    if not isinstance(limit, (int, float)):
+        raise TypeError("limit must be an int or float")
+    result = []
+    for i in range(len(bmi)):
+        if bmi[i] > limit:
+            result.append(True)
+        else:
+            result.append(False)
+    return result
---
+++ 1_array/ex00/tester.py
@@ -0,0 +1,21 @@
+from give_bmi import give_bmi, apply_limit
+
+def main():
+    # Exemple de données
+    height = [2.71, 1.15]
+    weight = [165.3, 38.4]  # volontairement pas la même taille pour tester l'erreur
+
+    try:
+        # Calcul du BMI
+        bmi = give_bmi(height, weight)
+        print("Liste des IMC :", bmi, type(bmi))
+
+        # Application d'une limite
+        limit_result = apply_limit(bmi, 26)
+        print("Résultats du filtre :", limit_result, type(limit_result))
+
+    except (TypeError, ValueError) as e:
+        print(f"Erreur : {e}")
+
+if __name__ == "__main__":
+    main()
