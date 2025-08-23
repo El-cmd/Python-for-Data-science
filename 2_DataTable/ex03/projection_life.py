011182c66708811c61bcef8446d11cb01d654704
El-cmd <lothvalentin@gmail.com>
2025-08-23 12:59:55 +0200

    ex03

--- 2_DataTable/ex03/projection_life.py
+++ 2_DataTable/ex03/projection_life.py
@@ -1,13 +1,29 @@
 import pandas as pd
 from load_csv import load
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 def main():
-    df = load("life_expectancy_years.csv")
-    df2 = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+    try:
+        df = load("life_expectancy_years.csv")
+        df2 = load("income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
+        plt.plot(df2['1900'], df['1900'], 'bo')
+        plt.title("1900")
+        plt.xlabel("Gross domestic product")
+        plt.ylabel("Life Expectancy")
     
-
+        # Formater l'axe X pour afficher 100, 1K, 10K, etc.
+        def format_income(x, pos):
+            if x >= 1000:
+                return f'{int(x/1000)}K'
+            else:
+                return f'{int(x)}'
+    
+        plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(format_income))
+        plt.show()
+        plt.close()
+    except Exception:
+        return
 
 if __name__ == "__main__":
     main()
-    
