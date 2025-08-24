bd7b1692c2ae406de081a1974fa4046e0d7831c2
El-cmd <lothvalentin@gmail.com>
2025-08-24 19:59:19 +0200

    ex00

---
+++ 3_OOP/ex00/S1E9.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod # abc permet de definir des classe de base abstraites
+
+class Character(ABC):
+    """Class abstraite Character"""
+    @abstractmethod
+    def __init__(self, name, is_alive=True):
+        """Initialise le nom et l'etat de vie de la classe abstraite"""
+        self.name = name
+        self.is_alive = is_alive
+    def die(self):
+        """Change l'etat de vie de la classe abstraite"""
+        self.is_alive = False
+        
+
+class Stark(Character):
+    """Class héritée de Character"""
+    def __init__(self, name, is_alive=True):
+        """Initialise le nom et l'etat de vie de la classe héritée"""
+        super().__init__(name, is_alive)
+    def die(self):
+        """Change l'etat de vie de la classe héritée"""
+        super().die()
